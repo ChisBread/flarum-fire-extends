c876519fa949fba1ff94c2566129348b940ed87e
ChisBread <chisbread@gmail.com>
2023-04-28 20:33:52 +0800

    update username rules

--- extend.php
+++ extend.php
@@ -15,6 +15,7 @@ return [
         }
         $has_min = false;
         $has_max = false;
+        // password限制
         $rules['password'] = array_map(function (string $rule) {
             if (Str::startsWith($rule, 'min:')) {
                 $has_min = true;
@@ -31,6 +32,26 @@ return [
         }
         if (!$has_max) {
             $rules['password'][] = 'max:12';
+        }
+        $has_min = false;
+        $has_max = false;
+        // username限制
+        $rules['username'] = array_map(function (string $rule) {
+            if (Str::startsWith($rule, 'min:')) {
+                $has_min = true;
+                return 'min:4';
+            }
+            if (Str::startsWith($rule, 'max:')) {
+                $has_max = true;
+                return 'max:12';
+            }
+            return $rule;
+        }, $rules['username']);
+        if (!$has_min) {
+            $rules['username'][] = 'min:4';
+        }
+        if (!$has_max) {
+            $rules['username'][] = 'max:12';
         }
         $validator->setRules($rules);
     }),
